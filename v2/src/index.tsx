44f0c7b5f238514e5501037ba7adf3fb39432713
Reynnan Viktor <r.cavalcante-alves@klarna.com>
2022-04-09 17:36:12 +0200

    Moving providers outside App

--- v2/src/index.tsx
+++ v2/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@fontsource/poppins';
 import { ThemeProvider } from '@mui/material/styles';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import RollbarClient from 'clients/RollbarClient';
+import { AuthProvider } from 'providers/AuthProvider';
 
 import './index.css';
 import App from './App';
@@ -13,10 +16,16 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RollbarProvider instance={RollbarClient}>
+        <ErrorBoundary>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
