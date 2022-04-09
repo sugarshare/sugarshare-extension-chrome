a17c274b8372cb2fd36c12a95f2d0ad1212e55eb
Reynnan Viktor <r.cavalcante-alves@klarna.com>
2022-04-09 17:40:40 +0200

    Creating Home and Login Page

---
+++ v2/src/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import Layout from 'components/Layout';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Upload from 'features/Upload';
+import { useAuth } from 'providers/AuthProvider';
+
+const CHROME_SIGNOUT = () => chrome.runtime.sendMessage({ action: 'signout' });
+
+const Wrapper = styled('article')(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+}));
+
+export default function HomePage() {
+  const { state: authState, dispatch } = useAuth();
+
+  return (
+    <Layout
+      centerLogo={false}
+      onClickLogout={() => {
+        CHROME_SIGNOUT();
+        dispatch({ type: 'SIGN_OUT' });
+      }}
+    >
+      <Wrapper>
+        <Typography variant='body1'>Welcome {authState.user}!</Typography>
+        <Upload />
+      </Wrapper>
+    </Layout>
+  );
+}
