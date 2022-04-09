a5d7561de38973fe39c405a5d99529fbfdb6eec1
Reynnan Viktor <r.cavalcante-alves@klarna.com>
2022-04-09 17:38:36 +0200

    App now handles if user goes to Home or Login page

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,61 +1,17 @@
-import React from 'react';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Tooltip from '@mui/material/Tooltip';
-import Link from '@mui/material/Link';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import HomeIcon from '@mui/icons-material/Home';
-
-import RollbarClient from 'clients/RollbarClient';
-
-import settings from './settings';
-
-import TabsView from './components/TabsView';
-import Upload from './features/Upload';
-import Account from './features/Account';
+import LoginPage from 'pages/LoginPage';
+import HomePage from 'pages/HomePage';
+import { useAuth } from 'providers/AuthProvider';
 
 export default function App() {
-  return (
-    <RollbarProvider instance={RollbarClient}>
-      {/*
-        TODO
-        fallbackUI
-      */}
-      <ErrorBoundary>
-        <Container maxWidth='sm'>
-          <Box sx={{ my: 4 }}>
-            <Link href={`https://${settings.siteDomainName}`} target='_blank'>
-              <Tooltip title={`Go to ${settings.siteDomainName}`}>
-                <img
-                  src='/images/banner-v3.png'
-                  alt={`${settings.projectName} logo`}
-                  aria-label={`${settings.projectName} logo`}
-                  style={{
-                    width: '200px',
-                    display: 'block',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                  }}
-                />
-              </Tooltip>
-            </Link>
-            <TabsView items={[
-              {
-                name: 'Home',
-                component: <Upload />,
-                icon: <HomeIcon />,
-              },
-              {
-                name: 'Account',
-                component: <Account />,
-                icon: <AccountCircleIcon />,
-              },
-            ]}
-            />
-          </Box>
-        </Container>
-      </ErrorBoundary>
-    </RollbarProvider>
-  );
+  const {state: authState} = useAuth();
+
+  if (
+    authState.user !== null &&
+    authState.isAuthenticated &&
+    authState.isSessionExpired === false
+  ) {
+    return <HomePage />;
+  }
+
+  return <LoginPage />;
 }
