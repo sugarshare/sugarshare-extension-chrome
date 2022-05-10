805ccf409174425eacee16febfa9a3583b2e41c5
Redouane Achouri <achouri.a.r@gmail.com>
2022-05-10 07:52:55 +0200

    Set endpoints for development environment

--- src/settings/project.ts
+++ src/settings/project.ts
@@ -1,9 +1,9 @@
 export default {
   projectName: 'sugarshare',
   decoratedProjectName: 'SugarShare',
-  environment: 'dev',
+  environment: 'development',
   rootDomainName: 'sugarshare.me',
-  siteDomainName: 'www.sugarshare.me',
-  apiDomainName: 'api.sugarshare.me',
-  authDomainName: 'auth.sugarshare.me',
+  siteDomainName: 'dev.sugarshare.me',
+  apiDomainName: 'dev-api.sugarshare.me',
+  authDomainName: 'dev-auth.sugarshare.me',
 } as const;
