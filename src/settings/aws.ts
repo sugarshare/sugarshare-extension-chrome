751b111b3e347c59625867e41d183a626b321e47
Redouane Achouri <achouri.a.r@gmail.com>
2022-05-22 11:10:48 +0200

    Update cognito user pool and client ID parameters

--- src/settings/aws.ts
+++ src/settings/aws.ts
@@ -1,10 +1,8 @@
-import project from './project';
-
 export default {
   // region: 'eu-west-1',
   cognito: {
-    userPoolId: 'eu-west-1_s1GTzjS1U',
-    userPoolWebClientId: '2h2qmuj7b5svm94sbg09rcvuig',
+    userPoolId: 'eu-west-1_ihVj52nz1',
+    userPoolWebClientId: '3lkcuakhgh84nc90kv7qbiml3b',
     redirectSignIn: chrome?.identity?.getRedirectURL(),
   },
 } as const;
